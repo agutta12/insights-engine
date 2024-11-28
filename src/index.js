1ea1c2c0c6ddd546fe8aab1b38c25b118fd142e2
ananthagutta <ananth.gutta@gmail.com>
2024-11-28 08:26:50 -0600

    Updated app.js with the app URL

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css'; // Import your styles
+import App from './app'; // Import your main component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
